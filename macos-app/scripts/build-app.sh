5b475fd9f0581b7098b235ddd554e926441bfca5
Sergio Farfan <sergio.farfan@gmail.com>
2026-08-11 18:01:47 -0600

    v1.1.0: version bump for the feature wave
    Batch rename, bulk-read speedup, free-form merge, unified Merge tab, and the cloud-playlist read fix since 1.0.0. CFBundleVersion 2. Installer: AppleMusicConsolidator-1.1.0.dmg (dist/, sha256 a13c541d...5802).

--- macos-app/scripts/build-app.sh
+++ macos-app/scripts/build-app.sh
@@ -89,9 +89,9 @@ cat > "${APP}/Contents/Info.plist" <<'PLIST'
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleShortVersionString</key>
-	<string>1.0.0</string>
+	<string>1.1.0</string>
 	<key>CFBundleVersion</key>
-	<string>1</string>
+	<string>2</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>14.0</string>
 	<key>LSApplicationCategoryType</key>
